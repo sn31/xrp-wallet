e3c8950903c88f5c5ec78f9ec98a0a4940a00f7d
SkyeN <skye@dames.es>
2018-12-07 11:22:48 -0800

    add content and style info component

--- src/components/IntroPanel.vue
+++ src/components/IntroPanel.vue
@@ -1,3 +1,46 @@
 <template>
-<h1>Stuff</h1>
+    <b-container class="bv-row">
+        <b-row id="panel">
+            <b-col lg="4"><img src="../assets/ripplenet-section.gif"/></b-col>
+            <b-col lg="8">
+                <h1>Meet RippleNet</h1>
+                <p>Ripple connects banks and payment providers via RippleNet to provide one frictionless experience for sending and receiving money globally.</p>
+                <ul>
+                    <li><img class="sm-logo" src="../assets/access.png"></li>
+                    <li><img class="sm-logo" src="../assets/cost.png"></li>
+                    <li><img class="sm-logo" src="../assets/certainty.png"></li>
+                    <li><img class="sm-logo" src="../assets/speed.png"></li>
+                </ul>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
+
+<style scoped>
+.bv-row {
+  padding: 20px 10px 20px 10px;
+}
+img {
+  width: 40%;
+}
+h1 {
+  padding-top: 30px;
+  color: #066b98;
+}
+p {
+  color: #808080;
+}
+#panel {
+  background-color: #e6eaee;
+}
+
+.sm-logo {
+  width: 30%;
+}
+ul {
+  list-style-type: none;
+}
+li {
+  display: inline-block;
+}
+</style>
