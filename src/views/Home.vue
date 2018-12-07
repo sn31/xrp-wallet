45979f8a7feee6217ee8c8d34d9fa8a075d6e246
SkyeN <skye@dames.es>
2018-12-07 10:56:21 -0800

    add bootstrap-vue and change component names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <XrpIntro msg="One frictionless experience to send money globally"/>
+    <Intro msg="One frictionless experience to send money globally"/>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/dl4OzGHNaGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <IntroPanel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import XrpIntro from "@/components/XrpIntro.vue";
-import XrpIntroPanel from "@/components/XrpIntroPanel.vue";
+import Intro from "@/components/Intro.vue";
+import IntroPanel from "@/components/IntroPanel.vue";
 export default {
   name: "home",
   components: {
-    XrpIntro,
-    XrpIntroPanel
+    Intro,
+    IntroPanel
   }
 };
 </script>
